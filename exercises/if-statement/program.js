e9c28b7ada08a780f336214c95c61b10fa56d4a7
E T <guofoo>
2015-06-18 18:13:48 +0000

    commit exercise content by Live4Code

--- exercises/if-statement/program.js
+++ exercises/if-statement/program.js
@@ -2,7 +2,7 @@ var fruit = 'orange';
 
 // enter your code below
 if (fruit.length> 5) {
-  console.log('The fruit name has mor characters.');
+  console.log('The fruit name has mor .');
 } else {
   console.log('The fruit name has five characters or less.');
 }
